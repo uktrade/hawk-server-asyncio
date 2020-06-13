9ce73879f25e60170203bf128a1f4235bf8acf17
Michal Charemza <michal@charemza.name>
2020-06-13 06:35:17 +0100

    (release) v0.0.8

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def long_description():
 
 setuptools.setup(
     name='hawk-server-asyncio',
-    version='0.0.7',
+    version='0.0.8',
     author='Department for International Trade',
     author_email='webops@digital.trade.gov.uk',
     description='Utility function to perform the server-side of Hawk '
